3da279c9995709e3aab3d6ca2fb87aa58f47abe5
Aniede Chibundu David <ndubest56@gmail.com>
2023-07-04 17:21:00 +0100

    using array mapping to display books Objects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
 import { Children } from "react";
 import "./index.css";
 
+const books = [
+  {
+    img: "./images/book-1.jpg",
+    title: "Iron Flame",
+    authur: "Rebecca Yarros",
+  },
+  {
+    img: "./images/book-2.jpg",
+    title: "The Housemaid",
+    authur: "Freida McFadden",
+  },
+  {
+    img: "./images/book-3.jpg",
+    title: "Unbroken Bonds of Battle: A Modern Warriors Book of Heroism",
+    authur: "Johnny Joey Jones",
+  },
+  {
+    img: "./images/book-4.jpg",
+    title: "Atomic Habit",
+    authur: "James Clear",
+  },
+  {
+    img: "./images/book-5.jpg",
+    title: "Outlive: The Science and Art of Longevity",
+    authur: "Peter Attia MD",
+  },
+  {
+    img: "./images/book-6.jpg",
+    title: "Verity",
+    authur: "Colleen Hoover",
+  },
+];
+
+const allBooks = books.map((book) => {
+  return (
+    <article className="book">
+      <img src={book.img} />
+      <h2>{book.title}</h2>
+      <h4>{book.authur}</h4>
+      {book.children}
+    </article>
+  );
+});
+
+const BookItems = () => {
+  return <>{allBooks}</>;
+};
+
 function Booklist() {
   return (
     <section className="booklist">
+      {/*display Books using Array of Objects*/}
+      <BookItems />
+
+      {/*display Books using props*/}
       <Book
         img="./images/book-1.jpg"
         title="Iron Flame"
@@ -16,22 +68,6 @@ function Booklist() {
         title="The Housemaid"
         authur="Freida McFadden"
       />
-      <Book
-        img="./images/book-3.jpg"
-        title="Unbroken Bonds of Battle: A Modern Warriors Book of Heroism"
-        authur="Johnny Joey Jones"
-      />
-      <Book
-        img="./images/book-4.jpg"
-        title="Atomic Habit"
-        authur="James Clear"
-      />
-      <Book
-        img="./images/book-5.jpg"
-        title="Outlive: The Science and Art of Longevity"
-        authur="Peter Attia MD"
-      />
-      <Book img="./images/book-6.jpg" title="Verity" authur="Colleen Hoover" />
     </section>
   );
 }
