d2ea1210c0419c81cb1a596fe7c8bb0f3b85c839
Aniede Chibundu David <ndubest56@gmail.com>
2023-07-07 13:24:22 +0100

    added function for restore all items

--- src/App.js
+++ src/App.js
@@ -2,87 +2,52 @@ import { useState } from "react";
 import { books } from "./books";
 //import Book from "./Book";
 
-const allBooks = books.map((book, index) => {
-  const displayTitle = () => {
-    alert(book.title);
+const Booklist = () => {
+  const [bookList, setBookList] = useState(books);
+
+  const deleAll = () => {
+    setBookList([]);
   };
-  //added getBook function
-  const getSingleBook = (id) => {
-    getBook(book.id);
+  const restoreAll = () => {
+    setBookList(books);
   };
+  const deleSingle = (id) => {
+    const newBooks = bookList.filter((book) => book.id !== id);
+    setBookList(newBooks);
+  };
+
+  const allBook = bookList.map((item) => {
+    const { id, tittle, img, authur } = item;
+    return (
+      <article className="book" key={id}>
+        <p className="number">{id}</p>
+        <img src={img} alt={tittle} />
+        <h3>{tittle}</h3>
+        <p>{authur}</p>
+        <button onClick={() => deleSingle(id)}>remove</button>
+      </article>
+    );
+  });
+
   return (
     <div>
-      <article className="book" key={book.id}>
-        <img src={book.img} />
-        <h2>{book.title}</h2>
-        <h4>{book.authur}</h4>
-        <button onClick={displayTitle}>Display title</button>
-        <button onClick={getSingleBook}>Get Authur</button>
-        <span className="number">{index + 1}</span>
-        {book.children}
-      </article>
-    </div>
-  );
-});
-
-const getBook = (id) => {
-  const theBook = books.find((book) => book.id === id);
-  console.log(theBook);
-  alert(theBook.authur);
-};
-
-const BookItems = () => {
-  const { book, setBook } = useState(books);
-  return (
-    <>
-      {allBooks} {getBook}
-    </>
-  );
-};
-function Booklist() {
-  return (
-    <>
       <div className="header">
-        <h1>Amazon Best Selling Books</h1>
+        <h1>Ikendu Book</h1>
         <p>
           A practice website done with Reactjs and CSS by Chibundu David Aniede
         </p>
       </div>
-
-      <section className="booklist">
-        {/*direct annoymous function to get books using Book component
-        {books.map((book, index) => (
-          <Book {...book} key={book.id} getBook={getBook} number={index} />
-        ))}*/}
-        {/*display Books using Array of Objects*/}
-        <BookItems />
-        {/*
-       using the constructor
-      {testingBook} 
-      */}
-
-        {/*
-        display Books using props*
-        <Book
-          id={1}
-          img="./images/book-1.jpg"
-          title="Iron Flame"
-          authur="Rebecca Yarros"
-          disPlayit={displayValue}
-        >
-          <p>I am a child prop</p>
-        </Book>
-        <Book
-          id={2}
-          img="./images/book-2.jpg"
-          title="The Housemaid"
-          authur="Freida McFadden"
-          getBook={getBook}
-        />
-        */}
-      </section>
-    </>
+      <section className="booklist">>{allBook}</section>
+      <div>
+        <button type="button" onClick={deleAll}>
+          Remove All
+        </button>
+        <button type="button" onClick={restoreAll}>
+          Restore All
+        </button>
+      </div>
+    </div>
   );
-}
+};
 
 export default Booklist;
